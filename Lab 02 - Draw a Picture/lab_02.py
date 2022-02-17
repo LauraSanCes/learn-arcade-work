b28df01bf22df90f3d8bc490ca18e1e453aceb2f
LauraSanchez <77784381+LauraSanCes@users.noreply.github.com>
2022-02-17 18:51:23 +0100

    Acabado

--- Lab 02 - Draw a Picture/lab_02.py
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -24,10 +24,25 @@ arcade.draw_lrtb_rectangle_filled(0, 800, 200, 0, arcade.color.DARK_GREEN)
 
 # --- Dibujo ovni ---
 
+
+
+
 arcade.draw_lrtb_rectangle_filled(400, 600, 210, 170, arcade.color.GRAY)
-arcade.draw_lrtb_rectangle_filled(250, 750, 250, 210, arcade.color.BLUE_GRAY)
+
+
+
+
+
+
+
 arcade.draw_lrtb_rectangle_filled(300, 700, 350, 250, arcade.color.GRAY)
 
+
+
+arcade.draw_circle_filled(500, 290, 65, arcade.color.DARK_SLATE_BLUE)
+
+
+arcade.draw_lrtb_rectangle_filled(250, 750, 250, 210, arcade.color.BLUE_GRAY)
 
 arcade.draw_circle_filled(300, 230, 20, arcade.color.DARK_YELLOW)
 
@@ -43,9 +58,24 @@ arcade.draw_circle_filled(650, 230, 20, arcade.color.DARK_YELLOW)
 
 arcade.draw_circle_filled(720, 230, 20, arcade.color.DARK_YELLOW)
 
+
+
+
 #Luna
 arcade.draw_circle_filled(750, 550, 40, arcade.color.RED_BROWN)
 
+
+#Bosque
+
+arcade.draw_rectangle_filled(100, 230, 20, 60, arcade.csscolor.SIENNA)
+
+arcade.draw_polygon_filled(((100, 300),
+                            (80, 260),
+                            (70, 220),
+                            (130, 220),
+                            (120, 260)
+                            ),
+                           arcade.csscolor.DARK_GREEN)
 
 
 
